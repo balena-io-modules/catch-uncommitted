9fec1668e5765990778d060272f4817700a0d962
Tim Perry <pimterry@gmail.com>
2017-09-14 15:47:46 +0200

    Make message phrasing a little more generic

--- catch-uncommitted.sh
+++ catch-uncommitted.sh
@@ -4,11 +4,11 @@ set -e
 echo
 
 if ! git diff HEAD --exit-code ; then
-	echo '** Uncommitted changes found after build - FAIL **'
+	echo '** Uncommitted changes found (^^^ diff above ^^^) - FAIL **'
 	exit 1
 elif test -n "$(git ls-files --exclude-standard --others | tee /dev/tty)" ; then
-	echo '** Untracked uncommitted files found after build (^^^ listed above ^^^) - FAIL **'
+	echo '** Untracked uncommitted files found (^^^ listed above ^^^) - FAIL **'
 	exit 2
 else
-	echo 'No unexpected changes after build, all good.'
+	echo 'No unexpected changes, all good.'
 fi
