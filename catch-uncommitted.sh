a0e62828e290f4f0827949d7ee97e637d7aa1238
Pagan Gazzard <page@balena.io>
2020-02-07 19:08:05 +0000

    Add --ignore-space-at-eol option to ignore eol differences
    Change-type: minor

--- catch-uncommitted.sh
+++ catch-uncommitted.sh
@@ -2,6 +2,7 @@
 set -e
 
 catch_no_git=0
+ignore_eol=''
 extra_args=''
 
 while [ $# -gt 0 ]; do
@@ -14,6 +15,9 @@ while [ $# -gt 0 ]; do
 		--skip-node-versionbot-changes)
 			extra_args="${extra_args} "":(exclude)package.json"" "":(exclude)package-lock.json"" "":(exclude)npm-shrinkwrap.json"" "":(exclude)CHANGELOG.md"" "":(exclude).versionbot/CHANGELOG.yml"""
 		;;
+		--ignore-space-at-eol)
+			ignore_eol="--ignore-space-at-eol"
+		;;
 	esac
 done
 
@@ -22,7 +26,7 @@ echo
 if [ $catch_no_git -eq 1 ] && !(hash git 2> /dev/null); then
 	echo "git not found, not running catch-uncommitted check"
 	exit 0
-elif ! git diff HEAD --exit-code -- . $extra_args ; then
+elif ! git diff HEAD --exit-code ${ignore_eol} -- . $extra_args ; then
 	echo '** Uncommitted changes found (^^^ diff above ^^^) - FAIL **'
 	exit 1
 elif test -n "$(git ls-files --exclude-standard --others | tee /dev/tty)" ; then
