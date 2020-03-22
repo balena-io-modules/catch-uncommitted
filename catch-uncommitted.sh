63070576676cb3b98640fc0bff48945cfe8339ff
Josh Bowling <josh@balena.io>
2020-03-22 13:48:22 +0900

    Remove /dev/tty pipe
    Change-type: patch
    Signed-off-by: Josh Bowling (josh@balena.io)

--- catch-uncommitted.sh
+++ catch-uncommitted.sh
@@ -39,7 +39,11 @@ if [ $catch_no_git -eq 1 ] && !(hash git 2> /dev/null); then
 elif ! git diff HEAD --exit-code ${ignore_eol} -- . "${extra_args[@]}" ; then
 	echo '** Uncommitted changes found (^^^ diff above ^^^) - FAIL **'
 	exit 1
-elif test -n "$(git ls-files --exclude-standard --others | tee /dev/tty)" ; then
+fi
+
+files="$(git ls-files --exclude-standard --others)"
+if test -n "$files" ; then
+	echo "$files"
 	echo '** Untracked uncommitted files found (^^^ listed above ^^^) - FAIL **'
 	exit 2
 else
